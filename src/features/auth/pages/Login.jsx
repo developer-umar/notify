d32bec643fdff993a802ba3ef3e34cbdc2fbc2f9
Mohammad Umar <mohammadumar8010@gmail.com>
2026-08-17 09:26:41 +0530

    updated routes

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
 
         try {
             await dispatch(loginUser(formData)).unwrap()
-            navigate('/notes', { replace: true });
+            navigate('/all-notes', { replace: true });
 
 
 
