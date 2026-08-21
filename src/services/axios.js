f4dda87cb4c29934cb57342e71d7e591c00e11a0
Mohammad Umar <mohammadumar8010@gmail.com>
2026-08-21 11:11:35 +0530

    removed headers

--- src/services/axios.js
+++ src/services/axios.js
@@ -7,10 +7,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://notrify.onrender.com/api/v1",
     withCredentials: true,               // it will sent cookies  automatically 
-    headers: {
-        "Content-Type": "application/json"
-
-    }
+    
 })
 
 // Separate axios instance for refresh request
