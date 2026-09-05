378890cdec6b3689aff6da223ed3af095c35ae3d
Mohammad Umar <mohammadumar8010@gmail.com>
2026-09-05 10:05:25 +0530

    added notification

--- src/features/notes/pages/NoteDetails.jsx
+++ src/features/notes/pages/NoteDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom';
@@ -7,6 +7,7 @@ import { BsBookmarkFill } from 'react-icons/bs';
 import { FiBookmark } from 'react-icons/fi';
 
 const NoteDetails = () => {
+    const { successDeleted, setsuccessDeleted } = useState(false); //state for handling popup notification after delete
     const { noteId } = useParams();
     const navigate = useNavigate();
     const { selectedNote, getNotebyId: { loading, error }, togglePinNote: { loading: pinLoading, error: pinError }, deletenote: { loading: deleteLoading, error: deleteError } } = useSelector((state) => state.notes);
@@ -27,8 +28,20 @@ const NoteDetails = () => {
     }
 
     const handleDeleteNote = async () => {
-        await dispatch(deleteNote(noteId)).unwrap();
-        navigate('/all-notes', { replace: true });
+        try {
+            await dispatch(deleteNote(noteId)).unwrap();
+            setsuccessDeleted(true);
+
+            setTimeout(() => {
+                navigate('/all-notes', { replace: true });
+            }, 1500);
+
+
+        } catch (error) {
+            console.log(error);
+
+        }
+
 
     }
 
@@ -50,85 +63,95 @@ const NoteDetails = () => {
 
 
     return (
-
-        <div className="max-w-3xl mx-auto p-4">
-
-            <div className="border p-5 rounded">
-
-                <div className="flex items-center justify-between">
-
-                    <h1 className="text-xl font-semibold">
-                        {selectedNote.title}
-                    </h1>
-
-                    <button
-                        type='button'
-                        onClick={handlePintoggle}
-                        disabled={pinLoading}
-
-                        title={
-                            selectedNote.isPinned ? "Unpin note" : "Pin note"
-                        }
-                        className={`p-2 rounded-full border ${selectedNote.isPinned
-                            ? "text-yellow-500"
-                            : "text-gray-500"
-                            }`}
-
-                    >
-                        {selectedNote.isPinned ? (<BsBookmarkFill size={20} />) :
-
-                            (<FiBookmark size={20} />)
-                        }
-
-                    </button>
-
-
-
-                </div>
-
-                {pinError && (
-                    <p className="text-red-500 mt-2">
-                        {pinError}
+        <div>
+            {successDeleted && (
+                <div className="fixed top-5 right-5 bg-white border shadow-lg rounded-lg px-5 py-3">
+                    <p className="font-medium text-green-600">
+                        Note deleted successfully
                     </p>
-                )}
+                </div>
+            )}
+
+            <div className="max-w-3xl mx-auto p-4">
+
+                <div className="border p-5 rounded">
+
+                    <div className="flex items-center justify-between">
+
+                        <h1 className="text-xl font-semibold">
+                            {selectedNote.title}
+                        </h1>
+
+                        <button
+                            type='button'
+                            onClick={handlePintoggle}
+                            disabled={pinLoading}
+
+                            title={
+                                selectedNote.isPinned ? "Unpin note" : "Pin note"
+                            }
+                            className={`p-2 rounded-full border ${selectedNote.isPinned
+                                ? "text-yellow-500"
+                                : "text-gray-500"
+                                }`}
+
+                        >
+                            {selectedNote.isPinned ? (<BsBookmarkFill size={20} />) :
+
+                                (<FiBookmark size={20} />)
+                            }
+
+                        </button>
 
 
-                {/* delete button */}
 
-                <div className=''>
+                    </div>
 
-                    <button
-                        type='button'
-                        onClick={handleDeleteNote}
-                        disabled={deleteLoading}
-                        className='border px-3 py-2 rounded'>
-
-
-                        {deleteLoading ? 'deleting note...' : ' delete note'}
-
-                    </button>
-
-                    {deleteError && (
+                    {pinError && (
                         <p className="text-red-500 mt-2">
-                            {deleteError}
+                            {pinError}
                         </p>
                     )}
 
 
+                    {/* delete button */}
+
+                    <div className=''>
+
+                        <button
+                            type='button'
+                            onClick={handleDeleteNote}
+                            disabled={deleteLoading}
+                            className='border px-3 py-2 rounded'>
+
+
+                            {deleteLoading ? 'deleting note...' : ' delete note'}
+
+                        </button>
+
+                        {deleteError && (
+                            <p className="text-red-500 mt-2">
+                                {deleteError}
+                            </p>
+                        )}
+
+
+
+                    </div>
+
+
+
+                    <div>
+
+                    </div>
+
+
+                    <p className="mt-4 whitespace-pre-wrap">
+                        {selectedNote.content}
+                    </p>
+
 
                 </div>
-
-
-
-                <div>
-
-                </div>
-
-
-                <p className="mt-4 whitespace-pre-wrap">
-                    {selectedNote.content}
-                </p>
-
 
             </div>
 
